1db738373fe1f01594dbe3c7cb650414416f80fc
HRH <cfent12@gmail.com>
2024-04-23 09:08:32 +0900

    Transport Mode 인증 관련 프로젝트 추가

---
+++ TransportModeClientApp/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TransportModeClientApp.TransportModeService;
+
+namespace TransportModeClientApp
+{
+    public partial class MainForm : Form
+    {
+        private CalculatorServiceClient client;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            client = new CalculatorServiceClient();
+            client.ClientCredentials.ClientCertificate.SetCertificate(
+                System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine,
+                System.Security.Cryptography.X509Certificates.StoreName.My,
+                System.Security.Cryptography.X509Certificates.X509FindType.FindByThumbprint, "9072de32578ea78e2e958ae2e01993cfa5b8dcc3");
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            double result = client.Add(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            double result = client.Subtract(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            double result = client.Multiply(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            double result = client.Divide(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            client.Close();
+        }
+    }
+}
---
+++ TransportModeHostConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using TransportModeServiceLib;
+
+namespace TransportModeHostConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8082/TransportModeService/CalculatorService");
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculatorService), new WSHttpBinding(), "CalculatorService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
